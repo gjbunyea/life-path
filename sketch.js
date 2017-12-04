dc67ed2f5bd1631e032b721fe3b425bb6d096c71
Greg Bunyea <greg.bunyea@gmail.com>
2017-12-04 05:00:22 -0500

    added sprites

--- sketch.js
+++ sketch.js
@@ -17,11 +17,32 @@ function setup(){
 	center = createSprite(width/2, height/2, 20, 20)
 	center.shapeColor = color(255,0,0)
 
+  life_components = ["love", "work","fitness","art","heartbreak","badwork","drinking"]
+
   love = Group();
   work = Group();
+  fitness = Group();
+  art = Group()
+  heartbreak = Group()
+  badwork = Group()
+  drinking = Group()
+
   love_arr = {"intell":0, "happ":5, "wealth":-5, "time":-10}
   work_arr = {"intell":5, "happ":-2, "wealth":10, "time":-7}
+  fitness_arr = {"intell":0, "happ":1, "wealth":0, "time":-1}
+  art_arr = {"intell":2, "happ":2, "wealth":-2, "time":-3}
+  heartbreak_arr = {"intell":-3, "happ":-5, "wealth":0, "time":-5}
+  badwork_arr = {"intell":-2, "happ":-4, "wealth":7, "time":-10}
+  drinking_arr = {"intell":-3, "happ":2, "wealth":-4, "time":-5}
  
+  love_img = loadImage("assets/love.png")
+  work_img = loadImage("assets/work.png")
+  fitness_img = loadImage("assets/fitness.png")
+  art_img = loadImage("assets/art.png")
+  heartbreak_img = loadImage("assets/heartbreak.png")
+  badwork_img = loadImage("assets/badwork.png")
+  drinking_img = loadImage("assets/drinking.png")
+
   spriteArray = [];
 
 }
@@ -47,6 +68,22 @@ function draw(){
       else if(work.contains(curr)){
         arr = work_arr
       }
+      else if(fitness.contains(curr)){
+        arr = fitness_arr
+      }
+      else if(art.contains(curr)){
+        arr = art_arr
+      }
+      else if(heartbreak.contains(curr)){
+        arr = heartbreak_arr
+      }
+      else if(badwork.contains(curr)){
+        arr = badwork_arr
+      }
+      else if(drinking.contains(curr)){
+        arr = drinking_arr
+      }
+      
       intell += arr.intell
       happ += arr.happ
       wealth += arr.wealth
@@ -59,31 +96,55 @@ function draw(){
   drawSprites();
   updatePlayer();
 
-  if(allSprites.length < 5){
-    drawRandomSprites();
+  while(allSprites.length < 5){
+    drawRandomSprite();
   }
 }
 
-function drawRandomSprites(){
+function drawRandomSprite(){
   var spr = createSprite(random(width),random(height),20,20); 
-
-  life_components = ["love", "work"]
-  n = random(life_components)
-  console.log(n)
-  switch(n){
+  
+  switch(random(life_components)){
     case "love":
-      console.log('love');
-      spr.shapeColor = color(252,133,237)
+      spr.addImage(love_img)
       spr.addToGroup(love)
       break;
     case "work":
-      console.log('work');
-      spr.shapeColor = color(148,231,116)
+      spr.addImage(work_img)
       spr.addToGroup(work)
       break;
+    case "fitness":
+      spr.addImage(fitness_img)
+      spr.addToGroup(fitness)
+      break;
+    case "art":
+      spr.addImage(art_img)
+      spr.addToGroup(art)
+      break;
+    case "heartbreak":
+      spr.addImage(heartbreak_img)
+      spr.addToGroup(heartbreak)
+      break;
+    case "badwork":
+      spr.addImage(badwork_img)
+      spr.addToGroup(badwork)
+      break;
+    case "drinking":
+      spr.addImage(drinking_img)
+      spr.addToGroup(drinking)
+      break;
   }
-  spr.life = 60; 
+  spr.life = round(random(60,100))
   
+/*  love = Group();
+  work = Group();
+  fitness = Group();
+  art = Group()
+  heartbreak = Group()
+  badwork = Group()
+  drinking = Group()
+  reading  = Group()
+*/
 }
 
 function updatePlayer() {
