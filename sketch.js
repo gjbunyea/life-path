bfdb6301af8f262b95d401e2cb05373469d46731
Greg Bunyea <greg.bunyea@gmail.com>
2017-12-04 03:17:40 -0500

    multiple life pieces working

--- sketch.js
+++ sketch.js
@@ -18,7 +18,10 @@ function setup(){
 	center.shapeColor = color(255,0,0)
 
   love = Group();
+  work = Group();
   love_arr = {"intell":0, "happ":5, "wealth":-5, "time":-10}
+  work_arr = {"intell":5, "happ":-2, "wealth":10, "time":-7}
+ 
   spriteArray = [];
 
 }
@@ -32,18 +35,24 @@ function draw(){
       "\nTime: "+time, 
       300,300);
 
-  player.attractionPoint(.6,width/2,height/2)
+  player.attractionPoint(.01,width/2,height/2)
 
   for (var i=0; i<allSprites.length; i++){
     if(player.overlap(allSprites[i])){
       curr = allSprites[i]
+      arr = ''
       if(love.contains(curr)){
-        intell += love_arr.intell
-        happ += love_arr.happ
-        wealth += love_arr.wealth
-        time += love_arr.time
+        arr = love_arr
       } 
-      allSprites[i].remove();
+      else if(work.contains(curr)){
+        arr = work_arr
+      }
+      intell += arr.intell
+      happ += arr.happ
+      wealth += arr.wealth
+      time += arr.time
+
+      curr.remove();
     }
   }
 
@@ -57,10 +66,24 @@ function draw(){
 
 function drawRandomSprites(){
   var spr = createSprite(random(width),random(height),20,20); 
-  spr.shapeColor = color(0,182,255)
+
+  life_components = ["love", "work"]
+  n = random(life_components)
+  console.log(n)
+  switch(n){
+    case "love":
+      console.log('love');
+      spr.shapeColor = color(252,133,237)
+      spr.addToGroup(love)
+      break;
+    case "work":
+      console.log('work');
+      spr.shapeColor = color(148,231,116)
+      spr.addToGroup(work)
+      break;
+  }
   spr.life = 60; 
   
-  spr.addToGroup(love)
 }
 
 function updatePlayer() {
